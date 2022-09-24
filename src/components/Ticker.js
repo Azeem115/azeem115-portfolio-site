e9c0bd6924a298cb88a08b58b7a0ceae9cf07cfd
Azeem115 <azeem.abbas115@gmail.com>
2022-09-24 20:50:50 +0500

    Installation and setting of React Ticker plugin

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,11 +1,11 @@
 import Ticker from 'react-ticker'
 
 const MoveStuffAround = () => (
-    <Ticker>
+    <Ticker speed={8} mode="chain" offset="run-in">
         {({ index }) => (
             <>
             <h1 className="is-size-3-mobile hero-heading-h1">
-                {/* {title} */}<span>UI Designer.</span><span>UX Designer.</span><span>Developer.</span><span className="heading-stroke-text">Your Digital Craftsman.</span>
+                {/* {title} */}<span>UI Designer.</span><span className="heading-italic-text">UX Designer.</span><span>Developer.</span><span className="heading-stroke-text">Your Digital Craftsman.</span>
           </h1>
             </>
         )}
