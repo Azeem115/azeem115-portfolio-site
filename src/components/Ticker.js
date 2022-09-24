a1478380ff53c79bf1b6bdf4d22d218b8e6cab9d
Azeem115 <azeem.abbas115@gmail.com>
2022-09-24 21:17:22 +0500

    Styling the ticker

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,15 +1,26 @@
 import Ticker from 'react-ticker'
 
 const MoveStuffAround = () => (
-    <Ticker speed={13} mode="chain" offset="20">
-        {({ index }) => (
-            <>
-            <h1 className="is-size-3-mobile hero-heading-h1">
-                {/* {title} */}<span>UI Designer.</span><span className="heading-italic-text">UX Designer.</span><span>Developer.</span><span className="heading-stroke-text">Your Digital Craftsman.</span>
-          </h1>
-            </>
-        )}
-    </Ticker>
+    <div>
+        <Ticker speed={13} mode="chain" offset="20">
+            {({ index }) => (
+                <>
+                <h1 className="is-size-3-mobile hero-heading-h1">
+                    <span>UI Designer.</span><span className="heading-italic-text">UX Designer.</span><span>Developer.</span>
+            </h1>
+                </>
+            )}
+        </Ticker>
+        <Ticker direction ="toRight" speed={13} mode="chain" offset="20">
+            {({ index }) => (
+                <>
+                <h1 className="is-size-3-mobile hero-heading-h1">
+                    <span className="heading-stroke-text">Your Digital Craftsman.</span><span className="heading-italic-text">Community Builder.</span>
+                </h1>
+                </>
+            )}
+        </Ticker>
+    </div>
 )
 
 export default MoveStuffAround
